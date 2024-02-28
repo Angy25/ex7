006fa2f9451027275fde3eb43ffe65f64827f4f9
Angy25 <angelina.ivashov@mail.huji.ac.il>
2024-02-28 17:12:21 +0200

    finished compare_2d_lists successfully

--- ex7.py
+++ ex7.py
@@ -41,11 +41,11 @@ def log_mult(x, y):
 def up(x, b, c, step):
     """
 
-    :param x: 
-    :param b: 
-    :param c: 
-    :param step: 
-    :return: 
+    :param x:
+    :param b:
+    :param c:
+    :param step:
+    :return:
     """
     # stop condition
     if c * b > x:
@@ -69,14 +69,14 @@ def zigzag(x, b, c, step):
         return c
     if not step:
         return None
-    
+
     # recursion
     if c * b > x:
         return zigzag(x, b, c - step, divide_by_2(step))
-    
+
     if c * b < x:
         return zigzag(x, b, c + step, divide_by_2(step))
-        
+
 
 def division(x, b):
     """
@@ -84,7 +84,7 @@ def division(x, b):
     step = 1
     c = 1
     c, step = up(x, b, c, step)
-    
+
     step = divide_by_2(divide_by_2(step))
     return zigzag(x, b, c, step)
 
@@ -104,6 +104,22 @@ def is_power(b: int, x: int) -> bool:
 
     # recursion
     return is_power(b, division(x, b))
+
+
+def reverse(s: str) -> str:
+    """
+    gets a string, the characters are in some order, the function crates a string with those characters in the opposite
+    order.
+    :param s: str
+    :return: str
+    """
+
+    # stop condition
+    if len(s) == 1:
+        return s
+
+    # recursion
+    return append_to_end(reverse(s[1::]), s[0])
 
 
 def play_hanoi(hanoi: Any, n: int, src: Any, dest: Any, temp: Any):
@@ -126,5 +142,69 @@ def play_hanoi(hanoi: Any, n: int, src: Any, dest: Any, temp: Any):
     play_hanoi(hanoi, n - 1, temp, dest, src)
 
 
+def count_1(num: int) -> int:
+    """counts how many times the digit 1 appears in num"""
+
+    # stop condition
+    if num == 1:
+        return 1
+    if num == 0:
+        return 0
+
+    # recursion
+    return add(count_1(num // 10), num % 10 == 1)
+
+
+def number_of_ones(n: int) -> int:
+    # stop condition
+    if n < 10:
+        return 1
+
+    # recursion
+    return add(count_1(n), number_of_ones(subtract_1(n)))
+
+
+def compare_2d_lists(l1, l2):
+    """
+    Recursively compares multy-dimensional lists.
+    :param l1: (list): First 2D list for comparison.
+    :param l2: (list): Second 2D list for comparison.
+    :return: True if the lists are equal, False otherwise.
+    """
+
+    if len(l1) != len(l2):  # Check if the lengths of the lists are equal
+        return False
+
+    if len(l1) == len(l2) == 0:  # Spacial case of empty list
+        return True
+
+    i = subtract_1(len(l1))  # Start from the last element
+    if isinstance(l1[i], list) and isinstance(l1[i], list):  # If elements are lists, recursively compare them
+        if not compare_2d_lists(l1[i], l2[i]):
+            return False
+        if compare_2d_lists(l1[i], l2[i]):
+            i = subtract_1(i)  # Move to the previous element for comparison
+            if i == -1:
+                return True
+            return compare_2d_lists(l1[i], l2[i])
+
+    else:  # Entered the most inner list (list of ints or empty list)
+        if l1[i] != l2[i]:  # Check if the current elements are equal
+            return False
+        if l1[i] == l2[i]:
+            i = subtract_1(i)  # Move to the previous element for comparison
+            if i == -1:
+                return True
+            return compare_2d_lists([l1[i]], [l2[i]])
+
+def magic_list(n):
+
+
+
+
+
 if __name__ == '__main__':
-    print(is_power(3, 28))
+    print(compare_2d_lists([[1, 3], [4]], [[1, 3], [4]]))
+
+
+
